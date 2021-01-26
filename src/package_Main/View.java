c124e54a2fc2476250feef0f5102df5b091a2f59
codss-ctrl <67316314+codss-ctrl@users.noreply.github.com>
2021-01-26 20:44:49 +0900

    회원 메인뷰 메뉴 추가

--- src/package_Main/View.java
+++ src/package_Main/View.java
@@ -169,7 +169,7 @@ public class View {
 				System.out.println("[3] 금액 충전");
 				System.out.println("[4] 공지 조회");
 				System.out.println("[5] 이용권 조회");
-				System.out.println("[6]         ");
+				System.out.println("[6] 도서 검색 ");
 				System.out.println("[7] 로그아웃");
 				System.out.println();
 				System.out.println("메뉴를 선택하세요");
@@ -191,7 +191,7 @@ public class View {
 		
 		// 대여목록 조회 -대여 상세 정보 조회 - 평점달기
 		
-		// 내정보 수정 -이름 변경, 비밀번호 변경, 회원  탈퇴
+		// 내정보 조회 -이름 변경, 비밀번호 변경, 회원  탈퇴
 		
 	
 		
