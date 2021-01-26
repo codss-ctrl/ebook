fc5024de14708a73c6336154016db1b597f1090e
codss-ctrl <67316314+codss-ctrl@users.noreply.github.com>
2021-01-26 19:31:47 +0900

    각각 메인뷰만

--- src/package_Main/View.java
+++ src/package_Main/View.java
@@ -27,6 +27,7 @@ public class View {
 	//로그인 - 관리자 main, 회원 main
 	
 	//회원가입 - id입력, pw 입력, name 입력, point 입력
+	
 	/**
 	 * 관리자 메인뷰 - 관리자 메서드	
 	 */
@@ -40,9 +41,25 @@ public class View {
 			System.out.println("[4] 공지 목록 조회");
 			System.out.println("[5] 이용권 조회");
 			System.out.println("[6] 매출 조회");
+			System.out.println("[7] 로그아웃");
 			System.out.println();
+			System.out.println("메뉴를 선택하세요");
+			if (!"".equals(message)) {
+				System.out.println();
+				System.out.println(message);
+				message = "";
+			}//end if
+		}	
+//			switch () {
+//			case value:
+//				
+//				break;
+//
+//			default:
+//				break;
+//			}
 			
-		}
+	}//end method
 		//대출 목록 조회
 		
 		//보유 도서 조회-도서 추가, 상세정보 수정,삭제
@@ -58,6 +75,27 @@ public class View {
 	/**
 	 * 	회원 메인뷰 - 회원 메서드
 	 */
+		private void userMainView() {
+			String message = "";
+			while (true) {
+//				showBanner("관리자 페이지");
+				System.out.println("[1] 대출 목록 조회");
+				System.out.println("[2] 내 정보 조회");
+				System.out.println("[3] 금액 충전");
+				System.out.println("[4] 공지 조회");
+				System.out.println("[5] 이용권 조회");
+				System.out.println("[6]         ");
+				System.out.println("[7] 로그아웃");
+				System.out.println();
+				System.out.println("메뉴를 선택하세요");
+				if (!"".equals(message)) {
+					System.out.println();
+					System.out.println(message);
+					message = "";
+				}//end if
+			}//end while
+		}//end method	
+			
 		//공지사항 확인 - 공지상세 정보 확인
 		
 		//돈 충전
