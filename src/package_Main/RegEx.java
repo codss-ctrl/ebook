1eb2080630dc585d4cc9ea79e64a9307f87d85df
castanea308 <78008307+castanea308@users.noreply.github.com>
2021-01-27 20:40:14 +0900

    .

--- src/package_Main/RegEx.java
+++ src/package_Main/RegEx.java
@@ -1,5 +1,31 @@
 package package_Main;
 
-public class RegEx {
+import java.util.regex.Pattern;
 
+
+public class RegEx {
+	
+	//4 ~ 12자리 영문 소문자, 숫자와 특수기호 _ , - 만 사용 가능합니다.
+	public static boolean checkUser_id(String id) {
+		String patternUId = "[a-z0-9_,-]{4,12}";
+		return Pattern.matches(patternUId, id);
+	}
+	
+	//8 ~ 15자리 영문 대 소문자, 숫자, 특수문자만 사용 가능합니다.
+	public static boolean checkUser_pw(String pw) {
+		String patternUPw = "\\S{8,15}";
+		return Pattern.matches(patternUPw, pw);
+	}
+	
+	//2~10자 한글만 사용 가능합니다. (특수기호, 공백 사용 불가)
+	public static boolean checkUser_name(String userName) {
+		String patternUName = "[가-힣]{2,10}";
+		return Pattern.matches(patternUName, userName);
+	}
+	
+	
+	public static boolean checkNotify_date() {
+		
+		return false;
+	}
 }
